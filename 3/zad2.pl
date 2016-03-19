8c066d06eadf7cf4f63d9b1d595277850c075617
Marcin Regulski <maarcin.regulski@gmail.com>
2016-03-19 14:09:50 +0100

    added 3/zad2
    solution uses a version of Kadane's algorithm
    with iteration simulated by recursion

--- 3/zad2.pl
+++ 3/zad2.pl
@@ -1,12 +1,30 @@
-higher(A, B, A) :-
-    A > B.
-higher(A,B,B) :-
-    A <= B.
-    
-max([X],X).
-max([X | Tail], Max) :-
-    max(Tail, Max2),
-    higher(Max2, X, Max).
+sum([],0).
+sum([Head|Tail], Sum) :-
+    sum(Tail, X),
+    Sum is X + Head.
 
+% convienence wrapper
+max_sum([], 0) :- !. % if list is empty don't go further
 max_sum(List, Sum) :-
-    !.
+    max_sum_(List,0, 0, Sum).
+
+
+% will be called with Y as 'sum so far'
+max_sum_([], _, Y, Y).
+
+% uses variation of Kadane's algorithm
+max_sum_([H|T], MaxEndHere, MaxSoFar, Sum) :-
+
+    NewMaxEndHere #= max(H, MaxEndHere + H),
+    NewMaxSoFar #= max(MaxSoFar, NewMaxEndHere),
+%    Sum1 #= max(NewMaxSoFar, NewMaxEndHere),
+
+%    write("H: "), write(H), nl,
+%    write("MaxEndHere: "), write(MaxEndHere),nl,
+%    write("MaxSoFar: "), write(MaxSoFar), nl,
+%    write("NewMaxEndHere: "), write(NewMaxEndHere),nl,
+%    write("NewMaxSoFar: "), write(NewMaxSoFar), nl,
+
+%    write("Sum1: "), write(Sum), nl,
+%    write("-----------"),nl,
+    max_sum_(T, NewMaxEndHere, NewMaxSoFar, Sum).
