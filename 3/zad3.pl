56982baf3617150011634c18ddbf60646e6372fc
Marcin Regulski <maarcin.regulski@gmail.com>
2016-03-20 15:30:16 +0100

    added explanation for sorting

--- 3/zad3.pl
+++ 3/zad3.pl
@@ -4,26 +4,33 @@
 %   List = odd permutation of N elements with N+1st element at even postion.
 % it only works if the list is sorted (or by defining inversions as relative
 % to original order)
+
+% SORTING: for number lists sortig is neccessary. Otherwise the program treats
+% the input list as sorted, and therefore number of inversions depends on the
+% ordering. E.g. [1,2,4,3] is odd, but calling even_permutation([1,2,4,3],X)
+% returns the same list as first X. This is incorrect if input is treated as
+% numbers, but correct if they are treated as ordered atoms.
+
 even_permutation( [], [] ).
-even_permutation( List, Perm ) :-
-    sort(List, [X|Tail]),
-    even_permutation( Tail, Perm1 ),
-    insert_odd_pos( X, Perm1, Perm ).
+even_permutation( [X|Tail], Even ) :-
+    %sort(List, [X|Tail]),
+    even_permutation( Tail, Odd ),
+    insert_odd_pos( X, Odd, Even ).
 
-even_permutation( List, Perm ) :-
-    sort(List, [X|Tail]),
-    odd_permutation( Tail, Perm1 ),
-    insert_even_pos( X, Perm1, Perm ).
+even_permutation( [X|Tail], Even ) :-
+    %sort(List, [X|Tail]),
+    odd_permutation( Tail, Odd ),
+    insert_even_pos( X, Odd, Even ).
 
-odd_permutation( List, Perm ) :-
-    sort(List, [X|Tail]),
-    odd_permutation( Tail, Perm1 ),
-    insert_odd_pos( X, Perm1, Perm ).
+odd_permutation( [X|Tail], Odd ) :-
+    %sort(List, [X|Tail]),
+    odd_permutation( Tail, Even ),
+    insert_odd_pos( X, Even, Odd ).
 
-odd_permutation( List, Perm ) :-
-    sort(List, [X|Tail]),
-    even_permutation( Tail, Perm1 ),
-    insert_even_pos( X, Perm1, Perm ).
+odd_permutation( [X|Tail], Odd ) :-
+    %sort(List, [X|Tail]),
+    even_permutation( Tail, Even ),
+    insert_even_pos( X, Even, Odd ).
 
 insert_odd_pos( X, List, [X|List] ).
 insert_odd_pos( X, [A, B|List], [A, B|NewList] ) :-
